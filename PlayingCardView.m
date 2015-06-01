b8899481f293dbfc631858f32b6d33ed78e7230a
zyq522376829 <522376829@qq.com>
2015-06-01 16:48:53 +0800

    添加了牌面以及翻转手势
    添加了J,Q,K的人头牌面，其他牌面则绘制出图形；还添加了翻转的手势，每次随机抽取一张牌；最后用代码添加了对牌面进行放大缩小的捏合手势。

--- PlayingCardView.m
+++ PlayingCardView.m
@@ -8,7 +8,30 @@
 
 #import "PlayingCardView.h"
 
+@interface PlayingCardView()
+@property (nonatomic) CGFloat faceCardScaleFactor;
+@end
+
 @implementation PlayingCardView
+
+#pragma mark - Properties
+
+// 注意同时用到setter方法和getter方法，就得synthesize自己
+@synthesize faceCardScaleFactor = _faceCardScaleFactor;
+
+#define DEFAULT_FACE_CARD_SCALE_FACTOR 0.90
+
+- (CGFloat)faceCardScaleFactor {
+    if (!_faceCardScaleFactor) {
+        _faceCardScaleFactor = DEFAULT_FACE_CARD_SCALE_FACTOR;
+    }
+    return _faceCardScaleFactor;
+}
+
+- (void)setFaceCardScaleFactor:(CGFloat)faceCardScaleFactor {
+    _faceCardScaleFactor = faceCardScaleFactor;
+    [self setNeedsDisplay];
+}
 
 - (void)setSuit:(NSString *)suit {
     _suit = suit;
@@ -23,6 +46,14 @@
 - (void)setFaceUp:(BOOL)faceUp {
     _faceUp = faceUp;
     [self setNeedsDisplay];
+}
+
+- (void)pinch:(UIPinchGestureRecognizer *)gesture {
+    if ((gesture.state == UIGestureRecognizerStateChanged) ||
+        (gesture.state == UIGestureRecognizerStateEnded)) {
+        self.faceCardScaleFactor *= gesture.scale;
+        gesture.scale = 1.0;
+    }
 }
 
 #pragma mark - Drawing
@@ -48,7 +79,105 @@
     [[UIColor blackColor] setStroke];
     [roundedRect stroke];
     
-    [self drawCorners];
+    if (self.faceUp) {
+        UIImage *faceImage = [UIImage imageNamed:[NSString stringWithFormat:@"%@%@", [self rankAsString], self.suit]];
+        if (faceImage) {
+            CGRect imageRect = CGRectInset(self.bounds,
+                                           self.bounds.size.width * (1.0 - self.faceCardScaleFactor),
+                                           self.bounds.size.height * (1.0 - self.faceCardScaleFactor));
+            [faceImage drawInRect:imageRect];
+        } else {
+            [self drawPips];
+        }
+        
+        [self drawCorners];
+    } else {
+        [[UIImage imageNamed:@"cardback"] drawInRect:self.bounds];
+    }
+}
+
+#pragma mark - Pips
+
+#define PIP_HOFFSET_PERCENTAGE 0.165
+#define PIP_VOFFSET1_PERCENTAGE 0.090
+#define PIP_VOFFSET2_PERCENTAGE 0.175
+#define PIP_VOFFSET3_PERCENTAGE 0.270
+
+- (void)drawPips {
+    if ((self.rank == 1) || (self.rank == 5) || (self.rank == 9) || (self.rank == 3)) {
+        [self drawPipsWithHorizontalOffset:0
+                            verticalOffset:0
+                        mirroredVertically:NO];
+    }
+    if ((self.rank == 6) || (self.rank == 7) || (self.rank == 8)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCENTAGE
+                            verticalOffset:0
+                        mirroredVertically:NO];
+    }
+    if ((self.rank == 2) || (self.rank == 3) || (self.rank == 7) || (self.rank == 8) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:0
+                            verticalOffset:PIP_VOFFSET2_PERCENTAGE
+                        mirroredVertically:(self.rank != 7)];
+    }
+    if ((self.rank == 4) || (self.rank == 5) || (self.rank == 6) || (self.rank == 7) || (self.rank == 8) || (self.rank == 9) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCENTAGE
+                            verticalOffset:PIP_VOFFSET3_PERCENTAGE
+                        mirroredVertically:YES];
+    }
+    if ((self.rank == 9) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCENTAGE
+                            verticalOffset:PIP_VOFFSET1_PERCENTAGE
+                        mirroredVertically:YES];
+    }
+}
+
+#define PIP_FONT_SCALE_FACTOR 0.012
+
+- (void)drawPipsWithHorizontalOffset:(CGFloat)hoffset
+                      verticalOffset:(CGFloat)voffset
+                          upsideDown:(BOOL)upsideDown {
+    if (upsideDown) {
+        [self pushContextAndRotateUpsideDown];
+    }
+    CGPoint middle = CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2);
+    UIFont *pipFont = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    pipFont = [pipFont fontWithSize:[pipFont pointSize] * self.bounds.size.width * PIP_FONT_SCALE_FACTOR];
+    NSAttributedString *attributedSuit = [[NSAttributedString alloc] initWithString:self.suit attributes:@{NSFontAttributeName : pipFont }];
+    CGSize pipSize = [attributedSuit size];
+    CGPoint pipOrigin = CGPointMake(middle.x - pipSize.width / 2.0 - hoffset * self.bounds.size.width,
+                                    middle.y - pipSize.height / 2.0 - voffset * self.bounds.size.height);
+    [attributedSuit drawAtPoint:pipOrigin];
+    if (hoffset) {
+        pipOrigin.x += hoffset * 2.0 * self.bounds.size.width;
+        [attributedSuit drawAtPoint:pipOrigin];
+    }
+    if (upsideDown) {
+        [self popContext];
+    }
+}
+
+- (void)drawPipsWithHorizontalOffset:(CGFloat)hoffset
+                      verticalOffset:(CGFloat)voffset
+                  mirroredVertically:(BOOL)mirroredVertically {
+    [self drawPipsWithHorizontalOffset:hoffset
+                        verticalOffset:voffset
+                            upsideDown:NO];
+    if (mirroredVertically) {
+        [self drawPipsWithHorizontalOffset:hoffset
+                            verticalOffset:voffset
+                                upsideDown:YES];
+    }
+}
+
+- (void)pushContextAndRotateUpsideDown {
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(context);
+    CGContextTranslateCTM(context, self.bounds.size.width, self.bounds.size.height);
+    CGContextRotateCTM(context, M_PI);
+}
+
+- (void)popContext {
+    CGContextRestoreGState(UIGraphicsGetCurrentContext());
 }
 
 - (NSString *)rankAsString {
